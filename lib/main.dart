ce7c2a1a347014cde310b8ab1bba8c84f209e8e1
HarshitaSadadekar <harshitasadadekar@gmail.com>
2021-06-19 22:09:34 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,99 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children:<Widget> [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/Naruto.jpg'),
+                ),
+                Text('kikii',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text('Web Developer',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                 SizedBox(height: 20.0, width: 150.0,
+                 child: Divider(
+                   color: Colors.teal[100],
+                 ),),
+                 Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color:Colors.teal,
+                    ),
+                    title: Text('1231497457',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  )
+                  ),
+
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color:Colors.teal,
+                    ),
+                    title: Text('kikii@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                      ),
+                    ),
+                  )
+                )
+              ],
+            ),
+          )
+        ),
+      );
+
+  }
+}
+
+
+
+
+// Row(
+// children: <Widget>[
+// Icon(Icons.mail,
+// size: 40.0,
+// color: Colors.teal[900],
+// ),
+// SizedBox(
+// width: 10.0,
+// ),
+// Text('kikii@gmail.com',
+// style: TextStyle(
+// color: Colors.teal[900],
+// ),
+// ),
+//
+// ],
+// ),
